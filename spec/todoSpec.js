33ffce28e4926791cf21875af9629fa029c3399c
Zachariah Reitano <zreitano@gmail.com>
2013-07-29 15:38:12 -0400

    Add task funtionality

--- spec/todoSpec.js
+++ spec/todoSpec.js
@@ -1,10 +1,11 @@
 describe ("Todo", function() {
 
   describe("todo clone method", function() {
-     var todoClone = clone(todoItem);
+
     });
 
     it("should be able to create a clone for TodoItem", function() {
+      var todoClone = clone(todoItem);
       expect(todoClone).toEqual(todoItem);
     });
 
--- src/todo.js
+++ src/todo.js
@@ -7,3 +7,34 @@ function clone(todoItem) {
   return new Clone();
 }
 
+// todoItem.text = function(text){
+//   return text;
+// };
+
+todoApp.newTask = function(text) {
+  var task = clone(todoItem);
+  task.text = text;
+  return task;
+};
+
+var addTaskButton;
+
+var newTask = function() {
+   var taskVal = document.getElementById("new-task-field").value;
+   var newTask = todoApp.newTask(taskVal);
+   var ulTag= document.getElementById("todo-items");
+   var liTag = document.createElement('li');
+   liTag.innerText = newTask.text;
+   ulTag.appendChild(liTag);
+};
+
+window.onload = function() {
+
+    addTaskButton = document.getElementById("add-item");
+
+    addTaskButton.onclick = function(event) {
+      newTask();
+    };
+
+};
+
