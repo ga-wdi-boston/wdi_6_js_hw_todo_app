e7ee2a32859d422a26c9f1977da659318f443b30
Zachariah Reitano <zreitano@gmail.com>
2013-07-29 16:33:21 -0400

    Completed Task button showing up

--- spec/todoSpec.js
+++ spec/todoSpec.js
@@ -2,12 +2,15 @@ describe ("Todo", function() {
 
   describe("todo clone method", function() {
 
-    });
+  });
 
     it("should be able to create a clone for TodoItem", function() {
       var todoClone = clone(todoItem);
       expect(todoClone).toEqual(todoItem);
     });
 
+  describe("todo add completed button method", function() {
+
+  });
 
 });
