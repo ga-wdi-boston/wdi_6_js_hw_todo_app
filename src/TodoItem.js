fb3023f95718feb45e54c08fbb8988a5d3cd34bd
Mike Stone <mikestone14@gmail.com>
2014-02-20 17:07:40 -0500

    Created functionality to add tasks, change them to completed, and delete them

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,3 +1,10 @@
-var TodoItem = function(){
-
+// You will have instances of TodoItem
+var TodoItem = function(body, options){
+	if (body.length < 1) {
+		throw new Error("Enter a valid task");
+		alert("Please insert a valid task.")
+	}
+	var options = options || {};
+	this.body = body;
+	this.created_at = Date.now();
 };
