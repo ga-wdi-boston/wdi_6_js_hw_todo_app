526e57d15e99b9ed663d7354f97a7fbd41eb7df0
Becky Bell <rlbell@alum.bu.edu>
2014-02-20 15:36:37 -0500

    added button to index.html, added function to save new item to unfinished list onclick

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,3 +1,26 @@
-var TodoApp = {
+window.onload = function() {
+	var button,
+			unfinished_list;
 
+	unfinished_list = document.getElementById('unfinished');
+
+	button = document.getElementById('add-item-button');
+	button.onclick = function(event) {
+		event.preventDefault();
+		TodoApp.add_to_list(unfinished_list);
+			return false;
+		}
 };
+
+var TodoApp = {
+	add_to_list: function(list) {
+	var new_line = document.createElement('li'),
+			new_item_text = document.getElementById('add-item');
+	new_line.innerHTML = new_item_text.value;
+	new_item_text.value = "";
+	if (new_line.innerHTML !=="") {
+		list.appendChild(new_line);
+	}
+	return true;
+}
+}
