dfe6293899f4edfde97a12892659c869ee0b864e
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-20 17:24:50 -0500

    add function of adding new unfinished item

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,3 +1,23 @@
 var TodoApp = {
-  
+
+  add_to_list: function(list) {
+    // get text of input field
+    new_item_text = document.getElementById('new-item');
+    // create new TodoItem with that text
+    var todoItem = new TodoItem(new_item_text.value);
+    // create new li element with text
+    new_li_unfini = document.createElement('li')
+    // insert li element into the page
+    new_button_delete_unfini = document.createElement('input')
+
+    new_li_unfini.innerHTML = todoItem.tex;
+    new_item_text.value = "";
+
+    if (new_li_unfini.innerHTML !== "" ){
+      list.appendChild(new_li_unfini);
+    };
+    return true;
+  }
 };
+
+
--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 window.onload = function() {
-  
   // Initialization
-  var todoItem = new TodoItem(),
-      button_create,
-      button_complete,
-      button_delete,
-      unfini_list;
-      fini_list;
 
-  unfini_list = document.getElementById('unfini_list');
+  TodoApp.button_create = document.getElementById('new-item-button');
+  TodoApp.unfini_list = document.getElementById('unfini-list')
   // Calling to create a new unfinished item
-  button_create = document.getElementById('new-item-button');
-
-  button_create.onclick = function(event){
+  TodoApp.button_create.onclick = function(event){
     event.preventDefault();
-    TodoApp.add_to_list(unfini_list);
+    TodoApp.add_to_list(TodoApp.unfini_list);
     return false;
   };
 
