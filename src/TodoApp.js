ce889012afea4c5c80aa690a99ebef31234755cc
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-20 15:10:07 -0500

    can now add items to unfinished list

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,3 +1,29 @@
+
+
+
+window.onload = function() {
+	var unfinished_tasks = document.getElementById('unfinished');
+	var create_button = document.getElementById('add-item');
+
+	create_button.onclick = function(event) {
+		event.preventDefault();
+		TodoApp.add_to_list(unfinished_tasks, 'new-task-field');
+		return false;
+	};
+};
+
+
 var TodoApp = {
 
 };
+TodoApp.add_to_list = function(list, type) {
+	var new_li = document.createElement('li');
+	var new_thing_to_add = document.getElementById(type);
+	new_li.innerHTML = new_thing_to_add.value;
+	new_thing_to_add.value = "";
+	if(new_li.innerHTML !== "") {
+		list.appendChild(new_li);
+	};
+	return false;
+};
+
