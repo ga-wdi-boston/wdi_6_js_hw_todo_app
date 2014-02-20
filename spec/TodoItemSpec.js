787797a44cc44225c6db4f26933268ee7105a870
Jordan Morano <jordan.morano@gmail.com>
2014-02-20 16:19:45 -0500

    Create new TodoItem with validation and shovel to unfinishedTasks array on TodoApp

--- spec/TodoItemSpec.js
+++ spec/TodoItemSpec.js
@@ -1,3 +1,22 @@
 describe('TodoItem', function(){
+  beforeEach(function(){
+    item = new TodoItem("homework")
+    item2 = new TodoItem("laundry")
+  });
 
+  describe("new", function(){
+    it("has a property called content", function(){
+      expect(item.content).toEqual("homework");
+    });
+
+    it("throws an error with 0 args", function(){
+      expect(function(){
+        new TodoItem();
+      }).toThrowError("invalid");
+    });
+
+    it("adds items to TodoApp unfinishedTasks array", function(){
+      expect(TodoApp.unfinishedTasks).toContain(item, item2);
+    });
+  });
 });
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,3 +1,7 @@
-var TodoItem = function(){
-
+var TodoItem = function(content){
+  if ( !content ) {
+    throw new Error("invalid");
+  }
+  this.content = content;
+  TodoApp.unfinishedTasks.push(this);
 };
