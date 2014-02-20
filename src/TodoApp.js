7f99adb7c9074c2f152c2a7e234e601f6b17a627
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-20 15:33:26 -0500

    now with no global vars

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,21 +1,11 @@
 
-
-
 window.onload = function() {
-	var unfinished_tasks = document.getElementById('unfinished');
-	var create_button = document.getElementById('add-item');
-
-	create_button.onclick = function(event) {
-		event.preventDefault();
-		TodoApp.add_to_list(unfinished_tasks, 'new-task-field');
-		return false;
-	};
+	TodoApp.create_unfinished_task();
 };
 
 
-var TodoApp = {
+var TodoApp = {};
 
-};
 TodoApp.add_to_list = function(list, type) {
 	var new_li = document.createElement('li');
 	var new_thing_to_add = document.getElementById(type);
@@ -26,4 +16,20 @@ TodoApp.add_to_list = function(list, type) {
 	};
 	return false;
 };
+
+TodoApp.create_unfinished_task = function(){
+	var unfinished_tasks = document.getElementById('unfinished');
+	var create_button = document.getElementById('add-item');
+	create_button.onclick = function(event) {
+		event.preventDefault();
+		TodoApp.add_to_list(unfinished_tasks, 'new-task-field');
+		return false
+	};
+};
+
+
+
+
+
+
 
