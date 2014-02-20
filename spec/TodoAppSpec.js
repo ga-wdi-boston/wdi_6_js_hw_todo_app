cd89e44eb237c7a7be4d120ab4f3fddbda371877
Jordan Morano <jordan.morano@gmail.com>
2014-02-20 16:11:26 -0500

    Add basic HTML structure and array properties to TodoApp

--- spec/TodoAppSpec.js
+++ spec/TodoAppSpec.js
@@ -1,3 +1,13 @@
 describe('TodoApp', function(){
 
+  describe("Properties:", function () {
+
+    it("an array called unfinishedTasks", function () {
+      expect(TodoApp.unfinishedTasks instanceof Array).toEqual(true);
+    });
+
+    it("an array called finishedTasks", function () {
+      expect(TodoApp.finishedTasks instanceof Array).toEqual(true);
+    });
+  });
 });
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,3 +1,4 @@
 var TodoApp = {
-
+  unfinishedTasks : [],
+  finishedTasks   : []
 };
