158213465ea73a5953149e8dfbd2e574c4e4bc33
Laure Fischer <laurefischer27@gmail.com>
2014-02-20 15:52:54 -0500

    added function to render new element for a new todo item, refactored function to append new item to todo list

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,26 +1,32 @@
 window.onload = function() {
   var list = document.getElementById('unfinished');
+  TodoApp.store_unfinished();
+  TodoApp.store_finished();
   button = document.getElementById('add-item');
   button.onclick = function(event) {
     event.preventDefault();
-    TodoApp.add_to_list(list);
+    var todo = new TodoItem(document.getElementById('new-task-field').value);
+    TodoApp.add_to_list(list, todo);
     return false;
   };
 };
-
 
 var TodoApp = {
 
 };
 
-TodoApp.add_to_list = function(list) {
-  var new_li = document.createElement('li'),
-  new_item_text = document.getElementById('new-task-field');
-  new_li.className = 'items';
-  new_li.innerHTML = new_item_text.value;
-  new_item_text.value = '';
-  list.appendChild(new_li);
+TodoApp.add_to_list = function(list, todo) {
+  var new_item = todo.render();
+  list.appendChild(new_item);
   return false;
+};
+
+TodoApp.store_unfinished = function() {
+  var unfinished_todos = document.getElementsByClassName('items');
+};
+
+TodoApp.store_finished = function() {
+  var finished_todos = document.getElementById('finished').children;
 };
 
 // TodoApp.add_to_bad_list = function(list) {
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,3 +1,12 @@
 var TodoItem = function(body){
   this.body = body;
 };
+
+TodoItem.prototype.render = function() {
+  var new_li = document.createElement('li'),
+  new_item_text = document.getElementById('new-task-field');
+  new_li.className = 'items';
+  new_li.innerHTML = new_item_text.value;
+  new_item_text.value = '';
+  return new_li;
+};
