416fd9160f95a9318c7ce5e4ed9def10a069ba85
Jordan Morano <jordan.morano@gmail.com>
2014-02-20 17:52:12 -0500

    Move create task functionality to TodoApp namespace

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,19 @@
 var TodoApp = {
   unfinishedTasks : [],
-  finishedTasks   : []
+  finishedTasks   : [],
+  addTodo         : function () {
+    var item, el, list, error;
+    item = document.getElementById('new-task-field');
+    list = document.getElementById('unfinished');
+    error = document.getElementById('error');
+    event.preventDefault();
+    try {
+      el = new TodoItem(item.value);
+      item.value = '';
+      error.innerHTML = '';
+      list.appendChild(el);
+    } catch (e) {
+      error.innerHTML = e;
+    }
+  }
 };
--- src/onload.js
+++ src/onload.js
@@ -1,18 +1,6 @@
 window.onload = function(){
   document.getElementById('add-item').onclick = function (event) {
-    var item, el, list, error;
-    item = document.getElementById('new-task-field');
-    list = document.getElementById('unfinished');
-    error = document.getElementById('error');
-    event.preventDefault();
-    try {
-      el = new TodoItem(item.value);
-      item.value = '';
-      error.innerHTML = '';
-      list.appendChild(el);
-    } catch (e) {
-      error.innerHTML = e;
-    }
+    TodoApp.addTodo();
   };
 
 };
