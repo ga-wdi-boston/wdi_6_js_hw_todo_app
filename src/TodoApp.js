c91a73d5502842cbeb152f237edb10341d8b8e96
K Mak <karen.mak@stx.oxon.org>
2014-02-20 14:25:05 -0500

    initialized app

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,3 +1,15 @@
-var TodoApp = {
+window.onload = function() {
+  var unfinished_list,
+      finished_list,
+      button;
 
-};
+  unfinished_list = document.getElementById('unfinished');
+  button = document.getElementById('')
+}
+
+  var TodoApp = {
+    add_to_list: function(list) {
+      var new_item = document.createElement('li');
+    }
+  }
+
