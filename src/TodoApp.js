85fa0a62149be60698a86ce8ff69968cd0ceb594
tsyan <anna.tsykalova@gmail.com>
2014-02-20 22:40:11 -0500

    delete button now works for unfinished list items

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,6 +1,5 @@
 window.onload = function() {
 	var add_item_button = document.getElementById('new-thing-button');
-			// done_button = document.getElementsbyClassName('done');
 
 	// add new item
 	add_item_button.onclick = function(event) {
@@ -9,21 +8,12 @@ window.onload = function() {
 		return false;
 	};
 
-	// mark item as done
-	// done_button.onclick = function(event) {
-	// 	event.preventDefault();
-	// 	TodoApp.move_to_finished_list;
-	// 	return false;
-	// };
-
 };
 
 var TodoApp = {
 	add_to_unfinished_list: function() {
 		var new_li = document.createElement('li'),
 				new_span = document.createElement('span'),
-				// new_done_button = document.createElement('button'),
-				// new_delete_button = document.createElement('button'),
 
 				new_item_text = document.getElementById('new-thing-input'),
 				unfinished_list = document.getElementById('unfinished-items');
@@ -35,40 +25,30 @@ var TodoApp = {
 			new_span.className = "item-text";
 			new_span.innerHTML = new_item_text.value;
 
-			var new_done_button = this.create_new_done_button();
-			// this.create_new_delete_button();
-
-			// new_done_button.className = "done";
-			// new_done_button.innerHTML = "done!";
-			// new_delete_button.className = "delete";
-			// new_delete_button.innerHTML = "delete";
+			var new_delete_button = this.create_new_delete_button();
 
 		  unfinished_list.appendChild(new_li);
 		  new_li.appendChild(new_span);
-		  new_li.appendChild(new_done_button);
-		  // new_li.appendChild(new_delete_button);
+		  new_li.appendChild(new_delete_button);
 
 			new_item_text.value = "";
 		};
 	},
 
-	// move_to_finished_list: function() {
-	// 	var current_button = document.getElementsbyClassName('done');
-	// },
 
-	create_new_done_button: function() {
-		var new_done_button = document.createElement('button');
+	create_new_delete_button: function() {
+		var new_delete_button = document.createElement('button');
 
-		new_done_button.className = "done";
-		new_done_button.innerHTML = "done!";
-		// this.parentNode.appendChild(new_done_button);
+		new_delete_button.className = "delete";
+		new_delete_button.innerHTML = "delete";
 
-		new_done_button.onclick = function(event) {
+		new_delete_button.onclick = function(event) {
 			event.preventDefault();
 			this.parentNode.parentNode.removeChild(this.parentNode);
+			return false;
 		};
 
-		return new_done_button;
+		return new_delete_button;
 	}
 
 
