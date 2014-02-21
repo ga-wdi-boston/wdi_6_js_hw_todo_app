41400e344416852b6b9efc2499bc4a712aa23e26
Laure Fischer <laurefischer27@gmail.com>
2014-02-20 20:36:57 -0500

    minor refactoring

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -29,12 +29,3 @@ TodoApp.store_unfinished = function() {
 TodoApp.store_finished = function() {
   return document.getElementById('finished').children;
 };
-
-// TodoApp.add_to_bad_list = function(list) {
-//   var bad_li = document.createElement('li'),
-//   bad_item_text = document.getElementById('new-bad-thing');
-//   bad_li.innerHTML = bad_item_text.value;
-//   bad_item_text.value = '';
-//   list.appendChild(bad_li);
-//   return false;
-// };
