a304df75f5e180ac423e6a52b7262302fdc5678b
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-02-21 10:55:23 -0500

    Got create new button to work (on my own, not using class review)

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -10,15 +10,13 @@ window.onload = function() {
   new_button = document.getElementById('new-task-button');
   new_button.onclick = function(event) {
     event.preventDefault();
-    MyApp.add_to_list(unfinished);
+    TodoApp.add_to_list(unfinished);
     return false;
   };
 };
 
+TodoApp = {};
 
-
-
-var TodoApp = {
   TodoApp.add_to_list = function(list) {
   var new_li = document.createElement('li');
       new_item_text = document.getElementById('new-task');
@@ -30,15 +28,15 @@ var TodoApp = {
   return true;
   }
 
-  // TodoApp.change_lists = function(list) {
-  // var add_done = document.createElement('li'),
-  //     new_tasked_text = document.getElementById('add-done');
-  // new_li.innerHTML = new_tasked_text.value;
-  // new_tasked_text.value = "";
-  // if (new_li.innerHTML !== "") {
-  //   list.appendChild(new_li);
-  // }
-  // return true;
-  // }
+// TodoApp.change_lists = function(list) {
+// var add_done = document.createElement('li'),
+//     new_tasked_text = document.getElementById('add-done');
+// new_li.innerHTML = new_tasked_text.value;
+// new_tasked_text.value = "";
+// if (new_li.innerHTML !== "") {
+//   list.appendChild(new_li);
+// }
+// return true;
+// }
 
-};
+// };
