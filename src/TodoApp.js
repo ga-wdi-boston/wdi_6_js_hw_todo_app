eaefd236d78c1a246258ec71cd993e2fb5f0fdf2
emreiser <emilie.reiser@gmail.com>
2014-02-21 00:56:15 -0500

    Added sorting of tasks by text or date created. Added icons to sort buttons.

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,11 +1,27 @@
 window.onload = function(){
-	var add_button, unfinished_list;
+	var add_button, unfinished_list, alpha_sort_button, date_sort_button;
 	add_button = document.getElementById('add-button');
 	unfinished_list = document.getElementById('unfinished');
+	alpha_sort_button = document.getElementById('alpha-sort-button');
+	date_sort_button = document.getElementById('date-sort-button');
 
 	add_button.onclick = function(event){
 		event.preventDefault();
 		TodoApp.newTodo(unfinished_list);
+		return false;
+	};
+
+	alpha_sort_button.onclick = function(event){
+		event.preventDefault();
+		TodoApp.sortArray('text');
+		TodoApp.renderLists();
+		return false;
+	};
+
+	date_sort_button.onclick = function(event){
+		event.preventDefault();
+		TodoApp.sortArray('date_created');
+		TodoApp.renderLists();
 		return false;
 	};
 };
@@ -16,6 +32,7 @@ TodoApp.tasks = [];
 
 TodoApp.renderLists = function() {
 	var i, length, finished_list, unfinished_list
+
 	i = 0;
 	length = this.tasks.length;
 	finished_list = document.getElementById('finished');
@@ -43,6 +60,20 @@ TodoApp.newTodo = function(list){
 	form_text.value = '';
 	this.tasks.push(item);
 	this.renderLists();
+};
+
+TodoApp.sortArray = function(field){
+
+	TodoApp.tasks.sort(function(a, b) {
+	  if (a[field] < b[field])
+	     return -1;
+	  if (a[field] > b[field])
+	     return 1;
+	  return 0;
+	});
+
+	return TodoApp.tasks;
+
 };
 
 
