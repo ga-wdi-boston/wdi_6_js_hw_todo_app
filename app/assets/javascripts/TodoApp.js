c7bb4ae9d6f586bee006bf9b306d34107fffc79f
Mike Talley <miketalley@hotmail.com>
2014-05-29 16:58:07 -0400

    Created new project and base HTML

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,23 @@
 var TodoApp = {
+  newTodo: function(event){
+    var newTodo = $('#todo-field').val();
 
+    if(newTodo !== ''){
+      var newRow = $('<tr>');
+      var newTodoCell = $('<td>').text(newTodo);
+      var newCreatedCell = $('<td>').text($.now());
+      var newDeleteCell = $("<td><input type='button' value='Delete' id='delete'>");
+      var newCompleteCell = $("<td><input type='button' value='Complete' id='complete'>");
+      newRow.append(newTodoCell, newCreatedCell, newDeleteCell, newCompleteCell);
+      $('#incomplete-tasks').append(newRow);
+      $('#todo-field').val('');
+    }
+    event.preventDefault();
+  },
+  completeTodo: function(event){
+
+  },
+  deleteTodo: function(event){
+
+  }
 };
--- app/assets/javascripts/app_ready.js
+++ app/assets/javascripts/app_ready.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-
+  $('#todo-input').on('submit', TodoApp.newTodo);
+  //$('#').on('click', 'li', function(){alert('This is the color ' + ($(this).text()));});
 });
