5f1a8aca92539a82e328ea4d85a7b93a0ac56411
Ethan Gould <ethan.g.907@gmail.com>
2014-05-29 16:36:04 -0400

    user can add item to 'Todo List'

--- app/assets/javascripts/app_ready.js
+++ app/assets/javascripts/app_ready.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-  $('#add-todo').on('click', function(){
-    alert('You did it!');
-  });
+  $('#add-todo').on('click', TodoApp.addTodo);
 });
+
+var TodoApp = {
+
+  addTodo: function(event){
+    var newTodo = $('#todo-field').val();
+    var newListItem = $('<li>').text(newTodo);
+    $('.todo-list').append(newListItem);
+    $('#todo-field').val('');
+    event.preventDefault();
+  }
+};
