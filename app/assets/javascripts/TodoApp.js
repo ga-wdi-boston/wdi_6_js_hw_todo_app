86ca3b6a478a119aa56180893385359f67ccad68
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-29 16:00:25 -0400

    Added descriptions to js files for reference

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,4 @@
+// This should handle inserting, relocating, and deleting elements from the DOM, and store an array of current `TodoItem` objects.
 var TodoApp = {
 
 };
--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -1,3 +1,9 @@
+// This represents an individual todo item, and handles constructing the DOM
+// elements for displaying itself.
+
+//These elements will need to include "complete" and "delete" buttons,
+// with appropriate functions bound to their click events.
+
 var TodoItem = function(){
 
 };
