99c409060b73f547dfa1c0c514f1e1bcd8aae732
George Straehle <gstraehle@gmail.com>
2014-05-29 16:49:48 -0400

    Allowed submit button to add todo list item
    blanks not allowed

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,11 +1,20 @@
 
 $(document).ready(function() {
-  $('#todo-input').on('submit', function(){
-    alert('You did it!');
-  });
+  $('#todo-input').on('submit', TodoApp.addTodoItem);
+
 });
 
 
 
 var TodoApp = {
+  addTodoItem: function(event){
+    var newTodo = $('#todo-field').val();
+
+    if(newTodo !== ''){
+      var newTodoItem = $('<li>').text(newTodo);
+      $('#todo-list').append(newTodoItem);
+      $('#todo-field').val('');
+    }
+    event.preventDefault();
+  }
 };
