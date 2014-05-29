827c3c7cb4efbcae7d4e6f6a667a292d4642d7b5
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-29 18:01:19 -0400

    Add "Complete" button to the to do list item.

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -3,10 +3,17 @@ var TodoApp = {
     var itemInput = $('#new-item').val();
     if (itemInput!== '') {
      var newToDo = new TodoItem(itemInput);
-     var newItem = $('<li>').text(newToDo.text);
+     var newItem = $('<li>').html(newToDo.date + " | " + newToDo.text + " | " + TodoApp.completeButton());
       $('#unfinished-list').append(newItem);
       $('#new-item').val('');
       event.preventDefault();
     }
+  },
+
+  completeButton: function(){
+    return '<a href="#" class="btn btn-small btn-default" id="complete-item-button">Complete</a>';
   }
 };
+
+
+
