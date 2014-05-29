28ca2f8ff2e7ce6ac08f5e5cd8892cbf351ef3c4
Ethan Gould <ethan.g.907@gmail.com>
2014-05-29 18:51:05 -0400

    New item can be added to ToDo list for real

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,13 @@
 var TodoApp = {
 
+  todoArray: [],
+
+  addNewItem: function(event){
+    var newTodo = new TodoItem($('#thingToDo').val());
+    newListItem = $('<li>').text(newTodo.name);
+    $('.todo-list').append(newListItem);
+    $('#thingToDo').val('');
+    TodoApp.todoArray.push(newTodo);
+    event.preventDefault();
+  }
 };
--- app/assets/javascripts/app_ready.js
+++ app/assets/javascripts/app_ready.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
-  $('#todo-field').on('submit', TodoApp.addTodo);
+  $('#todo-field').on('submit', TodoApp.addNewItem);
 });
-
-var TodoApp = {
-
-  addTodo: function(event){
-    var newTodo = $('#thingToDo').val();
-    var newListItem = $('<li>').text(newTodo);
-    $('.todo-list').append(newListItem);
-    $('#thingToDo').val('');
-    event.preventDefault();
-  }
-
-};
