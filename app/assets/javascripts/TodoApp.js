9c632ee5da97c777ebdcc32b2ccfe636ebb47a90
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-29 19:35:46 -0400

    Move the <li> construction into the TodoItem.js

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -8,23 +8,13 @@ var TodoApp = {
     var itemInput = $('#new-item').val();
     if (itemInput!== '') {
      var newToDo = new TodoItem(itemInput);
-     var newItem = $('<li>').html(newToDo.created_at + " | " + newToDo.text + " | " + TodoApp.completeButton() + " | " + TodoApp.deleteButton());
+     var newItem = newToDo.listElement();
      TodoApp.unfinished.push(newToDo);
      $('#unfinished-list').append(newItem);
       $('#new-item').val('');
       event.preventDefault();
     }
   },
-
-  completeButton: function(){
-    var newElement = $('<a>');
-    return '<a href="#" class="btn btn-small btn-default" id="complete-item-button">Complete</a>';
-  },
-
-  deleteButton: function(){
-    return '<a href="#" class="btn btn-small btn-default" id="delete-item-button">Delete</a>';
-  }
 };
-
 
 
--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -3,4 +3,16 @@ var TodoItem = function(text){
   this.text = text;
 };
 
+TodoItem.prototype.listElement = function(){
+  newElement = $('<li>');
+  newElement.html(this.created_at + " | " + this.text + " | " + this.completeButton() + " | " + this.deleteButton());
+  return newElement;
+};
 
+TodoItem.prototype.completeButton = function(){
+    return '<a href="#" class="btn btn-small btn-default" id="complete-item-button">Complete</a>';
+  };
+
+TodoItem.prototype.deleteButton = function(){
+    return '<a href="#" class="btn btn-small btn-default" id="delete-item-button">Delete</a>';
+  };
