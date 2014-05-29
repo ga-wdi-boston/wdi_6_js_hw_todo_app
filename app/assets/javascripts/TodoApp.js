d7efa59c26beefb4db049504431b1a155ed990a0
Ethan Gould <ethan.g.907@gmail.com>
2014-05-29 19:22:29 -0400

    when adding new item, ensures something is being added

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -4,10 +4,12 @@ var TodoApp = {
 
   addNewItem: function(event){
     var newTodo = new TodoItem($('#thingToDo').val());
-    newListItem = $('<li>').text(newTodo.name);
-    $('.todo-list').append(newListItem);
-    $('#thingToDo').val('');
-    TodoApp.todoArray.push(newTodo);
+    if(newTodo.name !== ''){
+      newListItem = $('<li>').text(newTodo.name);
+      $('.todo-list').append(newListItem);
+      $('#thingToDo').val('');
+      TodoApp.todoArray.push(newTodo);
+    }
     event.preventDefault();
   }
 };
