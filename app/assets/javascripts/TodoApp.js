f3b49718f0e7a41157094ac0e977140aed51f281
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-29 16:42:22 -0400

    Create "addNewItem" function within the TodoApp
    namespace.

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,12 @@
 var TodoApp = {
+  addNewItem: function(){
+    var itemInput = $('#new-item').val();
+    if (itemInput!== '') {
+      var newItem = $('<li>').text(itemInput);
 
+      $('#unfinished-list').append(newItem);
+      $('#new-item').val('');
+      event.preventDefault();
+    }
+  }
 };
