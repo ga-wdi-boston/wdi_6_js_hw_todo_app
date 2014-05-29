b0c038e1f5abfbae39ae9776ddbc44bf14153c27
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-29 18:53:21 -0400

    Add unfinished and finished arrays to TodoApp

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,16 +1,23 @@
 var TodoApp = {
+
+  unfinished: [],
+
+  finished: [],
+
   addNewItem: function(){
     var itemInput = $('#new-item').val();
     if (itemInput!== '') {
      var newToDo = new TodoItem(itemInput);
      var newItem = $('<li>').html(newToDo.date + " | " + newToDo.text + " | " + TodoApp.completeButton() + " | " + TodoApp.deleteButton());
-      $('#unfinished-list').append(newItem);
+     TodoApp.unfinished.push(newToDo);
+     $('#unfinished-list').append(newItem);
       $('#new-item').val('');
       event.preventDefault();
     }
   },
 
   completeButton: function(){
+    var newElement = $('<a>');
     return '<a href="#" class="btn btn-small btn-default" id="complete-item-button">Complete</a>';
   },
 
