301e35b6f4d7b69229a7879cfc7c05746870b8c6
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-29 16:47:22 -0400

    Added complete/delete on app

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,4 +1,8 @@
-// This should handle inserting, relocating, and deleting elements from the DOM, and store an array of current `TodoItem` objects.
-var TodoApp = {
+// This should handle inserting, relocating, and deleting elements from the
+// DOM, and store an array of current `TodoItem` objects.
 
+var TodoApp = {
+  taskList: [],
+  completeTask: function(){},
+  deleteTask: function(){}
 };
