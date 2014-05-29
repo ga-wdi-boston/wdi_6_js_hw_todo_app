caaaa1a35e693c9fa36429e18730d13f2a516018
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-29 16:47:02 -0400

    Set up title, and times created/completed on tasks

--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -4,6 +4,8 @@
 //These elements will need to include "complete" and "delete" buttons,
 // with appropriate functions bound to their click events.
 
-var TodoItem = function(){
-
+var TodoItem = function(title){
+  this.title = title;
+  this.createdAt = new Date();
+  this.completedAt = completedAt || '';
 };
