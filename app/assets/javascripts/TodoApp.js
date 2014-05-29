c8baf93b7a1852080b6b7a9eec6485bd8aa3b353
Christine Lafontant <Christine@Christines-MacBook-Pro.local>
2014-05-29 19:54:02 -0400

    Created specified event handler on Create button and corresponding function

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,12 @@
 var TodoApp = {
+  addNewTodo: function(event){
+    var newTodo = $('#to-do-field').val();
 
+    if (newTodo !== ''){
+      var newItem = $('<li>').text(newTodo).css('item', newItem);
+      $('#unfinished-to-dos').append(newItem);
+      $('#to-do-field').val('');
+    }
+    event.preventDefault();
+  }
 };
