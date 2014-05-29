f04c61f9a2bbbcb872fa212616c0277474c36e33
Lisa Plesko <plesko.l@gmail.com>
2014-05-29 17:10:35 -0400

    Basic html setup; user can add a todo; user can not add blank todo

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,17 @@
 var TodoApp = {
 
+  addNewItem: function(event){
+    var newTodo = $('#todo-field').val();
+    var newItem = $('<li>').text(newTodo);
+
+    if (newTodo === '') {
+      $('span').text( "Can't submit an empty field!" ).show().fadeOut( 1000 );
+      event.preventDefault();
+    } else {
+      $('#todo-list').append(newItem);
+      $('#todo-field').val('');
+      event.preventDefault();
+    }
+  }
+
 };
