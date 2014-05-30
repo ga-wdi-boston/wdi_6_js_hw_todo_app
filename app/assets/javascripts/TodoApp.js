2ed9d9fb4cc7af0bef7a9ee967a7beaf53ca154a
Ethan Gould <ethan.g.907@gmail.com>
2014-05-30 01:00:20 -0400

    added dynamic class name to <li>, started delete function

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -6,15 +6,22 @@ var TodoApp = {
     var newTodo = new TodoItem($('#thingToDo').val());
     if(newTodo.name !== ''){
       TodoApp.todoArray.unshift(newTodo);
-      var newListItem = $('<li>').text(TodoApp.todoArray[0].name);
-      $('.todo-list').append(newListItem);
+
+      var newListItem = ($('<li>').text(TodoApp.todoArray[0].name));
+      var listItemClass = TodoApp.todoArray[0].name;
+      var deleteButton = (' <button type="button" class="btn btn-danger btn-xs">Delete</button>');
+      var completeButton = (' <button type="button" class="btn btn-success btn-xs">Completed</button>');
+      newListItem.addClass(listItemClass);
+      $('.todo-list').append((newListItem).append(completeButton, deleteButton));
       $('#thingToDo').val('');
     }
     event.preventDefault();
   },
 
   deleteItem: function(event){
-
+    var btnClass = TodoApp.todoArray[0].name;
+    // var itemToDelete = ($('.todo-list'));
+    debugger;
     event.preventDefault();
   }
 };
