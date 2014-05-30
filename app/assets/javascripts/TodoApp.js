225322c85dc6135a208a52362d2998ff791a44eb
Ethan Gould <ethan.g.907@gmail.com>
2014-05-29 20:32:56 -0400

    Todo items are accessed through Array

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -5,11 +5,16 @@ var TodoApp = {
   addNewItem: function(event){
     var newTodo = new TodoItem($('#thingToDo').val());
     if(newTodo.name !== ''){
-      newListItem = $('<li>').text(newTodo.name);
+      TodoApp.todoArray.unshift(newTodo);
+      var newListItem = $('<li>').text(TodoApp.todoArray[0].name);
       $('.todo-list').append(newListItem);
       $('#thingToDo').val('');
-      TodoApp.todoArray.push(newTodo);
     }
+    event.preventDefault();
+  },
+
+  deleteItem: function(event){
+
     event.preventDefault();
   }
 };
