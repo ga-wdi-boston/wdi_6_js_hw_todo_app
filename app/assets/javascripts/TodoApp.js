c6c00a253189f46d47e432df1ec6312d33ddfe45
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-30 01:34:24 -0400

    Finally figured out how to create multiple/nested elements

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -11,9 +11,17 @@ var TodoApp = {
     if(newTaskTitle !== ''){
       TodoApp.unfinishedTasks.push(newTask);
       var newTask = new TodoItem(newTaskTitle);
+      // debugger;
 
-      // Trying to create the wrapper and the new ul here
-      var newItem = $('<ul>','<li>').text(newTaskTitle);
+      // Trying to create the li and p here
+      var newItem = $('<li>').attr('class', 'task').html([
+        $('<p>').attr('class','title').text(newTaskTitle),
+        $('<p>').attr('class','created-at').text('Created: ' + newTask.createdAt.toLocaleString()),
+        $('<p>').attr('class','make-complete').html(
+          $('<a>').attr('href', '#').text('Complete Task')
+          )
+        ]);
+
       $('#unfinished').append(newItem);
     }
   },
