40716c3e82d63dfdb07fca0ce7fe792fadcf8189
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-29 23:10:51 -0400

    Add event handler to "Complete" button on items

--- app/assets/javascripts/app_ready.js
+++ app/assets/javascripts/app_ready.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
   $('#create-new-item-button').click(TodoApp.addNewItem);
+  $('#unfinished-list').on('click', '#complete-item-button', function(){alert($(this).parent().text()); });
 });
