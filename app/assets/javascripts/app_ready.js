bec0bcbcaa7ea067b455831430b3f519789ea067
Ethan Gould <ethan.g.907@gmail.com>
2014-05-31 14:50:41 -0400

    delete button click function works appropriately

--- app/assets/javascripts/app_ready.js
+++ app/assets/javascripts/app_ready.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
   $('#todo-field').on('submit', TodoApp.addNewItem);
-  $('.btn-danger').on('click', TodoApp.deleteItem);
+  $('.todo-list').on('click', ('.btn-danger'), TodoApp.deleteItem);
 });
+
+// DOM manipulation documentation!
+
+// DOM .remove() to remove element, or .empty() to remove contents on <li>
+
+// findKeyword: function(keyword){
+//   return this.todos.filter(function(todo){ // todos is an array containing the list of todos
+//     return todo.indexOf(this.keyword) > -1; // returns true or false
+//   });
+// }
+
+// deleteing and item from array
+// people.filter(function(person) {return person.name === "alex";})[0]
