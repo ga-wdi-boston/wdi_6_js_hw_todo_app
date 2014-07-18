bd40379039970e3bc1de312159437a362350008f
csb324 <csb324@gmail.com>
2014-07-18 10:01:15 -0400

    First commit and oh boy it's a monster
    * Did so much stuff
    * Really

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,49 @@
 var TodoApp = {
+  currentItems: [],
+  finishedItems: [],
+
+  initialize: function() {
+
+    $('#clara-has-a-form').submit(this.createTask);
+
+  },
+
+  sortByName: function(list) {
+    return list.sort(function(a, b) {
+      return a.item.toLowerCase() - b.item.toLowerCase();
+    });
+  },
+
+  // this sorts by ascending (i think)
+  sortByDate: function(list) {
+    return list.sort(function(a, b) {
+      return a.createdAt - b.createdAt;
+    });
+  },
+
+  createTask: function(event) {
+    if($('#to-do-field').val() === ""){
+      event.preventDefault();
+      return;
+    }
+
+    var newTaskText = $('#to-do-field').val();
+    $('#to-do-field').val("");
+    var newTask = new TodoItem(newTaskText);
+    var newListItem = $('<li>');
+    newListItem.text(newTask.item);
+    $('.to-do-list').append(newListItem);
+
+    event.preventDefault();
+  },
+
+  // displayTask: function(todoitem) {
+  //   var newTask = $('<li>');
+  //   newTask.text(todoitem.item);
+  // },
+
+  // addNewTask: function() {
+  //   $('.to-do-list').append(this.displayTask(this.createTask()));
+  // }
 
 };
--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -1,3 +1,25 @@
-var TodoItem = function(){
+var TodoItem = function(item){
+  this.item = item;
+  this.finished = false;
+  this.createdAt = new Date();
+  TodoApp.currentItems.push(this);
+};
+
+TodoItem.prototype = {
+
+  markAsDone: function() {
+
+    this.removeItem();
+
+    this.finished = true;
+    this.finishedAt = new Date();
+
+    TodoApp.finishedItems.push(this);
+  },
+
+  removeItem: function() {
+    TodoApp.currentItems.splice(TodoApp.currentItems.indexOf(this), 1);
+  }
 
 };
+
