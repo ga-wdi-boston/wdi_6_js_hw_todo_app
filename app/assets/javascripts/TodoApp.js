a256f9ea55f92460ffcdbf1d97b72826e29fc342
Devin Dreszer <devindreszer@gmail.com>
2014-07-18 07:39:17 -0400

    User can add task to todo lists

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,16 @@
 var TodoApp = {
+  initialize: function() {
+    $('form').submit(this.addTask);
+  },
+  addTask: function(event) {
+    var newTask = $('#new-task-text').val();
 
+    if(newTask !== '') {
+      var tableRow = $('<tr>').text(newTask);
+      $('table').append(tableRow);
+      $('#new-task-text').val('');
+    }
+
+    event.preventDefault();
+  }
 };
