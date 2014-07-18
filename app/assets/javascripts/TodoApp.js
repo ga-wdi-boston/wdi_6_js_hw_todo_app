caeef6df2ed37e912166cfc5dc5c2f3b68f13adf
Jodi Detch <jodidetch@gmail.com>
2014-07-18 14:52:51 -0400

    Refactored addTask function so that placeholder text reappears after a new task is submitted

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -6,10 +6,11 @@ var TodoApp = {
 
   addTask: function(event){
     var newTask = $('#new-task-text').val();
+
     if (newTask.length > 0){
-    var listItem = $('<li>');
-    listItem.text(newTask);
+      var listItem = $('<li>').text(newTask);
       $('#incomplete-tasks-list').append(listItem);
+      $('#new-task-text').val('');
   }
   //often used with form submissions
   event.preventDefault();
