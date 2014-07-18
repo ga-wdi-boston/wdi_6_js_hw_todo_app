d2071041f5b142d514cac33218688b6065e043df
Devin Dreszer <devindreszer@gmail.com>
2014-07-18 16:05:03 -0400

    Users can sort by name

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -5,6 +5,8 @@ var TodoApp = {
     $('form').submit($.proxy(this.addTask, this));
     $('table').on('click', 'a[title="Complete"]', $.proxy(this.completeTask, this));
     $('table').on('click', 'a[title="Delete"]', $.proxy(this.deleteTask, this));
+    $('a[data-sort="task"]').click($.proxy(this.sortTasks, this));
+    $('a[data-sort="timestamps"]').click($.proxy(this.sortTasks, this));
   },
 
   addTask: function(event) {
@@ -43,6 +45,29 @@ var TodoApp = {
     return this.tasks.filter(function(task){
       return task.id === $(event.target).closest('tr').data('id');
     })[0];
+  },
+
+  sortTasks: function(event) {
+    // Map to sortBy attribute
+    var sortBy = $(event.target).data('sort');
+    var attributes = this.tasks.map(function(task){
+      return task[sortBy];
+    });
+
+    // Sort attributes
+    var sortedAttributes = attributes.sort();
+
+    // Sort tasks based off attribute sorting
+    sortedTasks = new Array(this.tasks.length);
+    this.tasks.forEach(function(task){
+      newIndex = sortedAttributes.indexOf(task[sortBy]);
+      sortedTasks[newIndex] = task;
+    });
+    this.tasks = sortedTasks;
+    this.regenerateLists();
+
+    $(event.target).parent().toggleClass('active');
+    event.preventDefault();
   },
 
   regenerateLists: function() {
--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -27,6 +27,9 @@ TodoItem.prototype = {
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
+    if(minutes < 10){
+      minutes = "0" + minutes;
+    }
     if(seconds < 10){
       seconds = "0" + seconds;
     }
