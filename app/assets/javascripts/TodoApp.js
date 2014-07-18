020c9c8e06d37d61bc2c0e8b5139b5f94d7b94f9
Jodi Detch <jodidetch@gmail.com>
2014-07-18 14:19:58 -0400

    App can now accept user input to add new to do item

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,17 @@
 var TodoApp = {
+  initialize: function(){
+  //event listeners must be inside initialize function
+  $('#new-task-form').submit(this.addTask);
+  },
 
+  addTask: function(event){
+    var newTask = $('#new-task-text').val();
+    if (newTask.length > 0){
+    var listItem = $('<li>');
+    listItem.text(newTask);
+      $('#incomplete-tasks-list').append(listItem);
+  }
+  //often used with form submissions
+  event.preventDefault();
+  }
 };
