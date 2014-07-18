06af567355a2b70a9d7af4f90d0a2aee61367ff4
Devin Dreszer <devindreszer@gmail.com>
2014-07-18 11:26:21 -0400

    Users can mark tasks as complete

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,16 +1,40 @@
 var TodoApp = {
+  tasks: [],
+
   initialize: function() {
-    $('form').submit(this.addTask);
+    $('form').submit($.proxy(this.addTask, this));
+    $('table').on('click', 'a', $.proxy(this.completeTask, this));
   },
+
   addTask: function(event) {
     var submission = $('#new-task-text').val();
     var newTask = new TodoItem(submission);
-
-    if(newTask !== '') {
-      $('table').append(newTask.html());
-      $('#new-task-text').val('');
-    }
+    this.tasks.push(newTask);
+    this.regenerateLists();
+    $('#new-task-text').val('');
 
     event.preventDefault();
+  },
+
+  completeTask: function(event) {
+    var currentTaskId = $(event.target).parents().find('tr').data('id');
+    var currentTask = this.tasks.filter(function(task){
+      return task.id === currentTaskId;
+    })[0];
+    currentTask.markCompleted();
+    this.regenerateLists();
+
+    event.preventDefault();
+  },
+
+  regenerateLists: function() {
+    $('table').empty();
+    this.tasks.forEach(function(task){
+      if(task.completed) {
+        $('[data-list="completed"]').find('table').append(task.html());
+      } else {
+        $('[data-list="active"]').find('table').append(task.html());
+      }
+    });
   }
 };
--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -1,9 +1,33 @@
 var TodoItem = function(task){
+  this.id = TodoItem.generateId();
   this.task = task;
+  this.completed = false;
+};
+
+TodoItem.nextId = 0;
+TodoItem.generateId = function(){
+  TodoItem.nextId += 1;
+  return TodoItem.nextId;
 };
 
 TodoItem.prototype = {
+  markCompleted: function() {
+    this.completed = true;
+    return this;
+  },
   html: function() {
-    return $('<tr>').text(this.task);
+    // Completed table data
+    var iconCompleted = $('<i>').addClass('fi-check');
+    var linkCompleted = $('<a>').attr('href', '#').addClass('title', 'Complete').append(iconCompleted);
+    var dataCompleted = $('<td>').append(linkCompleted);
+
+    // Task table data
+    var dataTask = $('<td>').text(this.task);
+
+    // Task table row
+    var tableRow = $('<tr>').append(dataCompleted);
+    tableRow.append(dataTask);
+    tableRow.data('id', this.id);
+    return tableRow;
   }
 };
