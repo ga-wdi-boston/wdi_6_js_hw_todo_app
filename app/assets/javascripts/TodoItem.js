4560abe9cd69293d4d1e2c19feeda322ac9e5082
Jodi Detch <jodidetch@gmail.com>
2014-07-20 19:26:28 -0400

    Tasks are now timestamped with created at and completed at times

--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -5,18 +5,20 @@ var TodoItem = function() {
   this.createdAt = new Date();
   this.completedAt = null;
   this.html = function(){
-    var htmlString = "<li>"+this.task+ " " +this.createdAt.toLocaleString();
+    var htmlString = "<li><div>"+this.task+"</div>";
+    htmlString = htmlString + "<div> Created at: " + this.createdAt.toLocaleString() + "</div>";
     var self = this;
 
     if(this.completed === false) {
-      htmlString = htmlString + '<button class="delete">Delete</button>';
       htmlString = htmlString + '<button class="completed">Completed</button>';
     } else {
-      htmlString  = htmlString + 'Completed at '+ this.completedAt.toLocaleString();
+      htmlString  = htmlString + '<div>Completed at: '+ this.completedAt.toLocaleString() + '</div>';
     }
-
+    htmlString = htmlString + '<button class="delete">Delete</button>';
     htmlString = htmlString + "</li>";
 
+    //Assign htmlObject to a jQuery object so you can use jQuery functions on it
+    // like 'find', etc.
     var htmlObject = $(htmlString);
 
     htmlObject.find('.delete').click(function(){
@@ -29,6 +31,8 @@ var TodoItem = function() {
       TodoApp.redrawLists();
     });
 
+    // You have to return the object so that the redrawLists function knows what
+    // to append to the appropriate list
     return htmlObject;
   };
 };
