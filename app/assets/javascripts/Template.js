adf72cf85e0ea684b3d20fb7552b942236c6c160
Blair Caple <blair@kaaterskil.com>
2014-07-20 15:38:34 -0400

    Moved DOM structure from js to view

--- app/assets/javascripts/Template.js
+++ app/assets/javascripts/Template.js
@@ -12,38 +12,6 @@ function Template(){
     '</div>' +
   '</li>';
 
-  this.baseTemplate =
-  '<div class="container-fluid">' +
-    '<div class="row">' +
-      '<input type="text" id="new-todo" placeholder="What would you like to do?" autofocus>' +
-      '<button id="todo-submit">Create</button>' +
-    '</div>' +
-    '<div class="row">' +
-      '<span>Total To-dos: </span>' +
-      '<span id="todo-counter"></span> | ' +
-      '<span><a href="#" id="sort-alpha">Sort by Title</a> | ' +
-      '<a href="#" id="sort-created">Sort by Creation Date</a> | ' +
-      '<a href="#" id="sort-completed">Sort by Completed Date</a></span>' +
-    '</div>' +
-    '<div class="row">' +
-      '<div class="col-xs-6">' +
-        '<div>' +
-          '<span>Active: </span>' +
-          '<span id="todo-active-counter"></span>' +
-        '</div>' +
-        '<ul id="todo-active-list"></ul>' +
-      '</div>' +
-      '<div class="col-xs-6">' +
-        '<div>' +
-          '<span>Completed: </span>' +
-          '<span id="todo-completed-counter"></span>' +
-        '</div>' +
-        '<ul id="todo-completed-list"></ul>' +
-      '</div>' +
-    '</div>' +
-  '</div>';
-}
-
 Template.prototype = {
   show : function(data, comparator){
     data = data || [];
--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,8 +1,6 @@
 var TodoApp = {
   init : function(){
     this.template = new Template();
-    $('h1').after(this.template.baseTemplate);
-
     this.storage = new Store();
     this.model = new Model(this.storage);
     this.view = new View(this.template);
